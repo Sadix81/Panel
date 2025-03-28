c920cf9c2373b22f9cb6424893e33b04c675fed9
SadraZargari <100510153+Sadix81@users.noreply.github.com>
2025-03-29 00:52:05 +0330

    fix routs

--- Modules/Faq/routes/api.php
+++ Modules/Faq/routes/api.php
@@ -4,9 +4,9 @@ use Illuminate\Support\Facades\Route;
 use Modules\Faq\Http\Controllers\FaqController;
 
 Route::prefix('/v1/faq/')->middleware('auth:api')->group(function () {
-    Route::get('/index', [FaqController::class, 'index']);
-    Route::post('/store', [FaqController::class, 'store']);
-    Route::get('/show/{faq}', [FaqController::class, 'show']);
-    Route::put('/update/{faq}', [FaqController::class, 'update']);
-    Route::delete('/destroy/{faq}', [FaqController::class, 'destroy']);
+    Route::get('/', [FaqController::class, 'index']);
+    Route::post('/', [FaqController::class, 'store']);
+    Route::get('/{faq}', [FaqController::class, 'show']);
+    Route::put('/{faq}', [FaqController::class, 'update']);
+    Route::delete('/{faq}', [FaqController::class, 'destroy']);
 });
--- Modules/Rating/routes/api.php
+++ Modules/Rating/routes/api.php
@@ -6,9 +6,9 @@ use Modules\Rating\Http\Controllers\RatingController;
 
 
 Route::prefix('/v1/rating')->middleware('auth:api')->group(function (){
-    Route::get('/index', [RatingController::class, 'index']);
-    Route::post('/store', [RatingController::class, 'store']);
-    Route::get('/show/detailes/{product}', [RatingController::class, 'show']);
+    Route::get('/', [RatingController::class, 'index']);
+    Route::post('/', [RatingController::class, 'store']);
+    Route::get('/detailes/{product}', [RatingController::class, 'show']);
     Route::put('/update/detail', [RatingController::class, 'update']);
     Route::get('/product/percent', [RatingController::class, 'rateCalculate']);
 });
