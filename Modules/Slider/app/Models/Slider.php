9a51fcc5b913ace56e0d68db6c95ad46f7d0f176
SadraZargari <100510153+Sadix81@users.noreply.github.com>
2025-04-04 17:05:33 +0330

    slider modules make

---
+++ Modules/Slider/app/Models/Slider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Modules\Slider\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Modules\Slider\Database\Factories\SliderFactory;
+
+class Slider extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        // 'image_url',
+        // 'image_type',
+        // 'image_size',
+    ];
+}
