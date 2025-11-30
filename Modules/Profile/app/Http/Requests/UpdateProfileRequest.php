e47d60b2414db80567062d65410650b7f886e7d6
SadraZargari <100510153+Sadix81@users.noreply.github.com>
2025-11-30 15:17:28 +0330

    fix profile

--- Modules/Profile/app/Http/Requests/UpdateProfileRequest.php
+++ Modules/Profile/app/Http/Requests/UpdateProfileRequest.php
@@ -7,23 +7,18 @@ use Illuminate\Validation\Rule;
 
 class UpdateProfileRequest extends FormRequest
 {
-    /**
-     * Get the validation rules that apply to the request.
-     */
     public function rules(): array
     {
         return [
-            'username' => ['nullable', 'max:100', 'string', Rule::unique('users')->ignore($this->user)],
-            'lastname' => ['nullable', 'max:100', 'string', Rule::unique('users')->ignore($this->user)],
-            'mobile' => ['nullable', 'regex:/[0-9]{10}/', 'digits:11', Rule::unique('users')->ignore($this->user)],
-            'email' => ['nullable', 'email', Rule::unique('users')->ignore($this->user)],
+            'username' => ['nullable', 'max:100', 'string', Rule::unique('users')->ignore($this->user()->id)],
+            'lastname' => ['nullable', 'max:100', 'string', Rule::unique('users')->ignore($this->user()->id)],
+            'mobile' => ['nullable', 'regex:/[0-9]{10}/', 'digits:11', Rule::unique('users')->ignore($this->user()->id)],
+            'email' => ['nullable', 'email', Rule::unique('users')->ignore($this->user()->id)],
             'avatar' => ['nullable', 'image', 'mimes:jpg,jpeg,png,bmp,gif,svg,webp', 'max:10240'], // Max size 10MB
+            'twofactor' => ['nullable', 'integer' ,  'in:0,1'],
         ];
     }
 
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
         return true;
