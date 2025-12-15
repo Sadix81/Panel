4cf8b8ca274409c128cadbe5aac36ad62a3502ba
SadraZargari <100510153+Sadix81@users.noreply.github.com>
2025-12-15 17:52:11 +0330

    creating weight module

---
+++ Modules/Weight/database/seeders/WeightDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Weight\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class WeightDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
