0d85641ae1135a9fe251e9355fc08ed90566982a
SadraZargari <100510153+Sadix81@users.noreply.github.com>
2025-12-15 18:42:19 +0330

    creating material

---
+++ Modules/Material/routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Modules\Material\Http\Controllers\MaterialController;
+
+/*
+ *--------------------------------------------------------------------------
+ * API Routes
+ *--------------------------------------------------------------------------
+ *
+ * Here is where you can register API routes for your application. These
+ * routes are loaded by the RouteServiceProvider within a group which
+ * is assigned the "api" middleware group. Enjoy building your API!
+ *
+*/
+
+Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
+    Route::apiResource('material', MaterialController::class)->names('material');
+});
